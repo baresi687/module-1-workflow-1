b7134583a4f7ccd442c73453e67d6efb6f45e4d3
hesh ramsis <heshamelmasry1992@gmail.com>
2022-10-20 21:48:15 +0200

    use vite

---
+++ day-4-environment-variables/end/js/main.js
@@ -0,0 +1,4 @@
+// By default, the dev server (dev command) runs in development mode and the build command runs in production mode.
+
+// console.log(process.env.VITE_API_KEY); // undefined
+console.log(import.meta.env.VITE_API_USER) // 123
