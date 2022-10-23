eb3f60adffaec20208c0b02d22748c853b2e5964
hreinn <hreinngy@gmail.com>
2022-10-23 18:56:32 +0200

    Adds .env files and stages

--- day-4-environment-variables/start/js/script.js
+++ day-4-environment-variables/start/js/script.js
@@ -1,1 +1,4 @@
-console.log(import.meta.env.VITE_NAME);
+const envVariable = import.meta.env.VITE_HELLO
+const mode = document.createElement('h2')
+mode.textContent = envVariable
+document.body.insertAdjacentElement('beforeend', mode)
